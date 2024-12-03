9e6a54756f261ce32abadee4a99b1abecccef6d5
ddnguyen <ddnguyen@smart-trade.net>
2024-12-03 15:15:04 +0100

    [Update]: train_ner.py bug fixed + implementation of smoothing rate

--- pstal/tp2/train_ner.py
+++ pstal/tp2/train_ner.py
@@ -17,7 +17,7 @@ sequoia_dev_path = os.path.join(project_path, "pstal/sequoia/sequoia-ud.parseme.
 sequoia_tiny_path = os.path.join(project_path, "pstal/sequoia/tiny.conllu")
 
 
-def train(file_path: str, count_display=False):
+def train(file_path: str, alpha=0.1, count_display=False):
     with open(file_path, "r", encoding="UTF-8") as infile:
         tokenLists = CoNLLUReader(infile).readConllu()
 
@@ -27,7 +27,7 @@ def train(file_path: str, count_display=False):
         c_s_t = Counter()  # Start-of-Sentence Tag Counts
 
         for tokenList in tokenLists:
-            forms = tokenList.metadata['text'].split()
+            forms = [token['form'] for token in tokenList]
             BIOs = CoNLLUReader.to_bio(tokenList, bio_style='bio', name_tag='parseme:ne')
 
             # Update start-of-sentence tag counts
@@ -38,13 +38,15 @@ def train(file_path: str, count_display=False):
                 c_w_t[form][bio] += 1
 
             # Update adjacent tag counts
-            for form, bio in zip(BIOs, BIOs[1:]):
-                c_t_t[form][bio] += 1
+            for tag1, tag2 in zip(BIOs, BIOs[1:]):
+                c_t_t[tag1][tag2] += 1
 
             # Update tag counts
             c_t.update(BIOs)
 
         S = sum(c_s_t.values())  # Total number of sentence
+        V_w = len(c_w_t.keys())
+        V_t = len(c_t.keys())
 
         if count_display:
             # Display results
@@ -63,24 +65,37 @@ def train(file_path: str, count_display=False):
             print("\nTotal number of sentences:")
             print(S)
 
+            print("\nSize of vocabulary:")
+            print(V_w)
+
+            print("\nNumber of labels:")
+            print(V_t)
+
         # Compute log probabilities
         log_E = defaultdict(dict)
+        for tag in c_t.keys():
+            log_E[tag]["OOV"] = Util.log_cap(c_t[tag] + V_w * alpha) - Util.log_cap(0 + alpha)
+
         log_T = defaultdict(dict)
+        for tag in c_t.keys():
+            log_T[tag]["OOV"] = Util.log_cap(c_t[tag] + V_t * alpha) - Util.log_cap(0 + alpha)
+
         log_pi = {}
+        log_pi["OOV"] = Util.log_cap(S + V_t * alpha) - Util.log_cap(0 + alpha)
 
         # Compute −logE(ti, wj)
         for word, tag_counts in c_w_t.items():
             for tag, count in tag_counts.items():
-                log_E[word][tag] = Util.log_cap(c_t[tag]) - Util.log_cap(count)
+                log_E[tag][word] = Util.log_cap(c_t[tag] + V_w * alpha) - Util.log_cap(count + alpha)
 
         # Compute −logT(ti, tj)
         for tag1, tag2_counts in c_t_t.items():
             for tag2, count in tag2_counts.items():
-                log_T[tag1][tag2] = Util.log_cap(c_t[tag1]) - Util.log_cap(count)
+                log_T[tag1][tag2] = Util.log_cap(c_t[tag1] + V_t * alpha) - Util.log_cap(count + alpha)
 
         # Compute −logπ(ti)
         for tag, count in c_s_t.items():
-            log_pi[tag] = Util.log_cap(S) - Util.log_cap(count)
+            log_pi[tag] = Util.log_cap(S + V_t * alpha) - Util.log_cap(count + alpha)
 
         # Convert defaultdicts to dicts for serialization
         log_E = {word: dict(tags) for word, tags in log_E.items()}
@@ -104,6 +119,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a model for NER")
     parser.add_argument("inputFileName", help="Path to the sequoia input file")
     parser.add_argument("outputFileName", help="Name and path for .pkl output file")
+
+    # TO DO : using optional parameters alpha and countDisplay
+    parser.add_argument("alpha", nargs='?', type=float, default=0.1, help="Smoothing rate")
     parser.add_argument("countDisplay", nargs='?', type=str, choices=["True", "False"], default="False", help="")
 
     args = parser.parse_args()
@@ -111,7 +129,14 @@ if __name__ == "__main__":
     # Get the input file name from the command line argument
     input_file_name = args.inputFileName
     output_path = args.outputFileName
+    alpha = args.alpha
     display = args.countDisplay == "True"  # Convert to boolean value
 
-    log_E, log_T, log_pi = train(input_file_name, count_display=display)
+    log_E, log_T, log_pi = train(input_file_name, alpha=alpha, count_display=display)
+
+    """
+    print(f"Log E: {log_E}\n")
+    print(f"Log T: {log_T}\n")
+    print(f"Log pi: {log_pi}\n")
+    """
     save(output_path, log_E, log_T, log_pi)
